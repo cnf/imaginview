ef5e74c812805e0a244fd2d8cd76cf8a8416ed55
frank.rosquin <frank.rosquin@9b3bd9c9-2926-0410-aa47-171fd0246937>
2007-01-03 22:51:09 +0000

    Initial import

---
+++ trunk/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<title>ImaginView Test Layout</title>
+<!--link rel="stylesheet" href="stylesheets/style.css" type="text/css"-->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#222222" text="white" vlink="sliver" link="silver">
+	<h2>Development Layout</h2>
